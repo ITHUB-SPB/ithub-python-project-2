35ffb5aaecbdfe68cc1736e52ccc396a9702ae9f
psengx <psengx@gmail.com>
2026-03-28 22:51:23 +0300

    что-то наделала

--- python_kt_2/web/app.py
+++ python_kt_2/web/app.py
@@ -22,9 +22,12 @@ def main():
         destination = Path() / 'python_kt_2' / 'web' / 'media' / text_file.filename
         text_file.save(destination)
 
+        if text_file.filename[-3:] != "txt":
+            return jsonify(error="Неверный формат файла. Пожалуйста, загрузите текстовый файл с расширением .txt"), 400
+
         text = destination.read_text(encoding="utf-8")
         result = stats_use_case(text, pos)
         
-        return jsonify(result)
+        return render_template("stats_result.html", result=result)
 
     app.run(debug=True)
