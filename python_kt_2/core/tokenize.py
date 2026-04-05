cb2bdcbf234c2fb7aedf57ce16090e87dc526e0a
Fisher Artur <damesom@mail.ru>
2026-04-05 16:54:07 +0300

    new

--- python_kt_2/core/tokenize.py
+++ python_kt_2/core/tokenize.py
@@ -1,21 +1,9 @@
 import re
 from .types import Tokens
 
-def _get_paragraphs(text: str) -> list[str]:
-    """Разбиение текста на параграфы (абзацы)."""
-    paragraphs = re.split(r'\n\s*\n', text)
-    return [p.strip() for p in paragraphs if p.strip()]
-
-
-def _get_sentences(text: str) -> list[str]:
-    """Разбиение текста на предложения."""
-    sentences = re.split(r'[.!?…]+', text)
-    sentences = [s.strip() for s in sentences if s.strip()]
-    return sentences
-
 
 def _get_words(text: str) -> list[str]:
-    """Разбиение текста на слова."""
+    """Разбиение на слова (с удалением пунктуации)."""
     words = re.findall(r'[а-яА-Яa-zA-Z0-9]+', text)
     words = [word.lower() for word in words]
     return words
@@ -29,6 +17,15 @@ def tokenize_text(text: str) -> Tokens:
     - предложения,
     - слова
     """
+
+    def _get_paragraphs(text: str) -> list[str]:
+        return text.split('\n\n')
+
+    def _get_sentences(text: str) -> list[str]:
+        sentences = re.split(r'[.!?…]+', text)
+        sentences = [s.strip() for s in sentences if s.strip()]
+        return sentences
+
     return {
         "paragraphs": _get_paragraphs(text),
         "sentences": _get_sentences(text),
